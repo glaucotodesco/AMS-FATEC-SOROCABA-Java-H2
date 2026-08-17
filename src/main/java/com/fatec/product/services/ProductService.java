469b82c2d0963ccb3f1b8f39fee9a68bf6ad2b89
glaucotodesco <glauco.todesco@gmail.com>
2026-08-17 13:21:19 -0300

    aula 10/08

---
+++ src/main/java/com/fatec/product/services/ProductService.java
@@ -0,0 +1,31 @@
+package com.fatec.product.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fatec.product.entities.Product;
+import com.fatec.product.repositories.ProductRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository repository;
+
+    ProductService(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+     public Product findById(Long id) {
+        return repository.findById(id)
+                         .orElseThrow(() -> new EntityNotFoundException());
+    }
+    
+}
